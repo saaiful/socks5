0f775a37d21effd600de77724497566064ab3cf1
Saiful Islam <saaifulislam@gmail.com>
2023-11-15 14:59:02 +0600

    UFW & IPTABLE Fix
    This will check for UFW & IPTABLE for port 1080 and if needed enable/open it.

--- socks5.sh
+++ socks5.sh
@@ -33,6 +33,18 @@ EOF'
 sudo useradd --shell /usr/sbin/nologin $username
 echo "$username:$password" | sudo chpasswd
 
+# Check if UFW is active and open port 1080 if needed
+if sudo ufw status | grep -q "Status: active"; then
+    sudo ufw allow 1080/tcp
+fi
+
+# Check if iptables is active and open port 1080 if needed
+if sudo iptables -L | grep -q "ACCEPT     tcp  --  anywhere             anywhere             tcp dpt:1080"; then
+    echo "Port 1080 is already open in iptables."
+else
+    sudo iptables -A INPUT -p tcp --dport 1080 -j ACCEPT
+fi
+
 # Restart dante-server
 sudo systemctl restart danted
 
